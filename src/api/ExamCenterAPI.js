494e765cf45e4f45b912d06093a34de5fda83131
Umar Mohammed <omar.m@comsoftltd.com>
2017-10-22 15:16:57 +0100

    exam center api cleaned up

--- src/api/ExamCenterAPI.js
+++ src/api/ExamCenterAPI.js
@@ -2,47 +2,51 @@ import { HTTP } from '../resources/resources'
 
 export default {
     CREATE_EXAMCENTER: async (examcenter) => {
-      const req =  new Promise ((resolve, reject) => {
-        HTTP.post('add/examcenter/', examcenter)
-        .then(response => { resolve(response.data) })
-        .catch(e => { reject(e) })
-      })
-      
-      const res = await req
-      return res
+      const res = () => {
+        return new Promise ((resolve, reject) => {
+          HTTP.post('add/examcenter/', examcenter)
+          .then(response => { resolve(response.data) })
+          .catch(e => { reject(e) })
+        })
+      }
+
+      const res = await res()
     },
 
     UPDATE_EXAMCENTER: async (examcenter) => {
-      const req = new Promise((resolve , reject) => {
-        HTTP.PUT(`update/examcenter/`, examcenter)
-        .then(response => { resolve(response.data) })
-        .catch(e => { reject(e); })
-      })
-      
-      const res =  await req
-      return res
+      const res = () => {
+        return new Promise((resolve , reject) => {
+          HTTP.PUT(`update/examcenter/`, examcenter)
+          .then(response => { resolve(response.data) })
+          .catch(e => { reject(e); })
+        })
+      }
+
+      return await res()
     },
 
     DELETE_EXAMCENTER: async (id) => {
-      const req = new Promise((resolve, reject) => {
-        HTTP.delete(`delete/examcenter/` + id)
-        .then(response => { resolve(response.data) })
-        .catch(e => { reject(e) })
-      })
+      const res = () => {
+        return new Promise((resolve, reject) => {
+          HTTP.delete(`delete/examcenter/` + id)
+          .then(response => { resolve(response.data) })
+          .catch(e => { reject(e) })
+        })
+      }
       
-      const res = await req
-      return res
+      return await res()
     },
 
     GET_EXAMCENTER: async (id) => {
-      const req = new Promise((resolve, reject) => {
-        HTTP.get(`examcenter/` + id)
-        .then(response => { resolve(response.data) })
-        .catch(e => { reject(e) })
-      })
-      
-      const res = await req
-      return res
+      const res = () => {
+        return new Promise((resolve, reject) => {
+          HTTP.get(`examcenter/` + id)
+          .then(response => { resolve(response.data) })
+          .catch(e => { reject(e) })
+        })
+      }
+       
+      return await res()
     },
 
     GET_EXAMCENTERS: async (stateid) => {
