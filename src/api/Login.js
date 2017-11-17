0a41f5026aad9a5ec3d7c16dddaa47baf3d1c730
Muhammad <muhammad.s@comsoftltd.com>
2017-11-17 11:02:33 +0100

    login/working

--- src/api/Login.js
+++ src/api/Login.js
@@ -6,8 +6,8 @@ export default {
     LOGIN: async (user) => {
         const res = () => {
           return new Promise((resolve, reject) => {
-            HTTP.get(`user/`, user)
-            .then(response => { resolve(response) })
+            HTTP.get(`user`, {params: {userid : user.userid,password :user.password }})
+            .then(response => { resolve(response.data) })
             .catch(e => { reject(e) })
           })
         }
